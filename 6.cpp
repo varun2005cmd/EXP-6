4e88fce5cb5d2d8375e6fc41ebe0e2069bb4c601
varun2005cmd <136258677+varun2005cmd@users.noreply.github.com>
2024-07-30 15:21:16 +0530

    Create 6.cpp

---
+++ 6.cpp
@@ -0,0 +1,20 @@
+// Varun Pendem
+// PRN: 23070123149
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   for(int i =0 ; i<100;i++){
+    cout<<endl<<i;
+   }
+   int j = 0;
+   while(j <5){
+    cout<<endl<<j<<endl;
+    j++;
+   }
+
+
+
+    
+}
