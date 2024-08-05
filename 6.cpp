c9d185dfbbe7767840a7df708628530548b1398c
varun2005cmd <136258677+varun2005cmd@users.noreply.github.com>
2024-08-05 23:48:01 +0530

    Update 6.cpp

--- 6.cpp
+++ 6.cpp
@@ -3,18 +3,93 @@
 
 #include <iostream>
 using namespace std;
-int main()
-{
-   for(int i =0 ; i<100;i++){
-    cout<<endl<<i;
-   }
-   int j = 0;
-   while(j <5){
-    cout<<endl<<j<<endl;
-    j++;
-   }
+
+int main() {
+    int s[100][100];
+    for (int i = 0; i < 100; ++i) {
+        for (int j = 0; j < 100; ++j) {
+            s[i][j] = i * 100 + j;
+        }
+    }
+
+    for (int i = 0; i <= 10; i++) {
+        cout << i << endl;
+    }
 
 
+    int x = 0;
+    cout << "While loop" << endl;
+    while (x < 10) {
+        cout << x << endl;
+        x++;
+    }
 
-    
+    int y = 0;
+    cout << "Do-while loop" << endl;
+    do {
+        y++;
+        cout << y << endl;
+    } while (y < 4);
+
+
+    int a = 0, b = 0;
+    cout << "Nested for loop" << endl;
+    for (int i = 0; i < 4; i++) {
+        a++;
+        for (int j = 0; j < 4; j++) {
+            b++;
+            cout << s[a][b] << " ";
+        }
+        cout << endl;
+    }
+
+    int k=0,l=0;
+    while(k<5)
+    {
+        while(l<5)
+        {
+            k++;
+            l++;
+            cout<<k<<endl;
+            cout<<l<<endl;
+        }
+    }
+
+    //pattern
+
+    for(int i = 0;i<=5;i++)
+    {
+        for(int j = 1;j<= 5-i;j++)
+        {
+            cout<<" ";
+        }
+        for(int k = 1;k<=2*i-1;k++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    int q = 0;
+
+    while(q<5)
+        {
+            for(int i = 0;i<5;i++)
+                {
+                    cout<<"printed"<<endl; 
+                }
+                q++;
+        }
+     
+
+    for(int i =0;i<10;i++)
+    {
+        while(i<5)
+        {
+            cout<<"printed"<<endl;
+            i++; //prints world hello 5 times 
+        }
+    }   
+
+    return 0;
 }
